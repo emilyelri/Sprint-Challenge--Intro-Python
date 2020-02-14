1c065ca962d5db0b5605bf7f96392e03520211a2
emilyelri <richard.emily.e@gmail.com>
2020-02-14 17:32:03 -0500

    finished oop challenges

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle():
+    #base class
+    pass
+
+class FlightVehicle():
+    #base class
+    pass
+
+class Starship():
+    #base class
+    pass
+
+class GroundVehicle(Vehicle):
+    #child class
+    pass
+
+class Airplane(FlightVehicle):
+    #child class
+    pass
+
+class Car(GroundVehicle):
+    #grandchild class
+    pass
+
+class Motorcycle(GroundVehicle):
+    #grandchild class
+    pass
